94e3d67bd58c9bcf9a3a51c3275ca47340f28b3c
Documenter.jl <documenter@juliadocs.github.io>
2024-07-27 11:44:50 +0000

    build based on d66e9bd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MATH371","category":"page"},{"location":"#MATH371","page":"Home","title":"MATH371","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MATH371.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MATH371]","category":"page"}]
+}
