d66e9bd11f8a0ea2b024a1ae1fe694ca671b4f76
Mohammed Alshahrani <mmogib@gmail.com>
2024-07-27 14:42:49 +0300

    initial one

--- src/MATH371.jl
+++ src/MATH371.jl
@@ -1,5 +1,9 @@
 module MATH371
 
 # Write your package code here.
+    function say_hi(name::String)
+        "Hi $name"
+    end
 
+    export say_hi
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "MATH371.jl" begin
     # Write your tests here.
+    name = "Mohammed"
+    @test say_hi(name) == "Hi Mohammed"
 end
